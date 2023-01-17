04c0599b78326b9ac9c6fd32585a5d3585fc52d5
Mehmet Ali Mergen <77547122+mehmetali10@users.noreply.github.com>
2023-01-17 14:19:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const e = require("express");
+
 
 const app = express();
 
